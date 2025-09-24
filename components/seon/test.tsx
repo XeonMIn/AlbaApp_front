65efe322148f7a307e4e093d0e6c9c88d2f1464c
leehosub <hosub9090@gmail.com>
2025-09-24 13:46:34 +0900

    테스트 수정

---
+++ components/seon/test.tsx
@@ -0,0 +1,15 @@
+import {View} from "react-native";
+import React from "react";
+import {Text } from "react-native";
+
+const test = () => {
+    return (
+            <View>
+
+                <Text>
+                    테스트용
+                </Text>
+
+            </View>
+    );
+}
